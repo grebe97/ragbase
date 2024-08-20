0e9c53593cb8aefa4d4ef719d3d4b532fb8270bb
Alexis Grebe <alexis.grebe.97@gmail.com>
2024-08-19 22:59:34 -0300

    cambios de confi

--- ragbase/config.py
+++ ragbase/config.py
@@ -15,15 +15,15 @@ class Config:
     class Model:
         EMBEDDINGS = "BAAI/bge-base-en-v1.5"
         RERANKER = "ms-marco-MiniLM-L-12-v2"
-        LOCAL_LLM = "gemma2:9b"
-        REMOTE_LLM = "llama-3.1-70b-versatile"
+        LOCAL_LLM = "llama3.1"
+        REMOTE_LLM = ""
         TEMPERATURE = 0.0
         MAX_TOKENS = 8000
-        USE_LOCAL = False
+        USE_LOCAL = True
 
     class Retriever:
         USE_RERANKER = True
         USE_CHAIN_FILTER = False
 
     DEBUG = False
-    CONVERSATION_MESSAGES_LIMIT = 6
+    CONVERSATION_MESSAGES_LIMIT = 1000000
